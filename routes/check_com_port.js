04b5003066e2f5db78b03a9830f04279f5a17e68
EC2 Default User <ec2-user@ip-172-31-16-82.ec2.internal>
2017-02-27 16:30:44 +0000

    modified:   data_loader/turnstiles/serialport-terminal.js 	modified:   routes/check_com_port.js

--- routes/check_com_port.js
+++ routes/check_com_port.js
@@ -7,21 +7,15 @@ var Port_checker=require('../data_loader/turnstiles/serialport-terminal.js');
 
 
 router.get('/test', function(req, res, next) {
-	
-
 		
 		data="ticket1"
 		results=[]
 		results.start=('checking database connection')	
-	
 		port_checker= new Port_checker("COM1")
 		port_checker.open_port(function (result){
-		
-		console.log(result)
+		console.log('open_port',result)	
 			return res.json(result);
 		})
-	
-
 })
 
 
