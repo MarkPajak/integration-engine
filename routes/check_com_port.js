4307cf786ff58711afbcb77a3ddbef8038c2256d
EC2 Default User <ec2-user@ip-172-31-16-82.ec2.internal>
2017-02-27 17:09:32 +0000

    modified:   routes/check_com_port.js

--- routes/check_com_port.js
+++ routes/check_com_port.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var Port_checker=require('../data_loader/turnstiles/serialport-terminal.js');
 
 
 
 
 router.get('/test', function(req, res, next) {
 		
-		data="ticket1"
-		results=[]
-		results.start=('checking database connection')	
-		port_checker= new Port_checker("COM1")
-		port_checker.open_port(function (result){
-		console.log('open_port',result)	
-			return res.json(result);
-		})
+var SerialPort = require('serialport');
+var port = new SerialPort('COM2', { autoOpen: false });
+
+port.open(function (err) {
+  if (err) {
+     return res.json(err.messag); 
+  }
+  // write errors will be emitted on the port since there is no callback to write
+  port.write('main screen turn on');
+});
+	
+	
 })
 
 
