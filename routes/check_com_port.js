1fdfcffd58493837fe2de0494ab26e3806a5f137
EC2 Default User <ec2-user@ip-172-31-16-82.ec2.internal>
2017-02-28 10:51:56 +0000

    modified:   routes/check_com_port.js

--- routes/check_com_port.js
+++ routes/check_com_port.js
@@ -9,16 +9,19 @@ router.get('/test', function(req, res, next) {
 var port =  req.app.get('port');
 
 console.log(port)
-port.open(function (err) {
-  if (err) {
-     return res.json(err.messag); 
-  }
-  // write errors will be emitted on the port since there is no callback to write
-  port.write('main screen turn on');
-  port.close(res.json('ok'))
-   
-  
+port.on('open', function() {
+  port.write('main screen turn on', function(err) {
+    if (err) {
+      return  return res.json(err.messag); 
+    }
+    console.log('message written');
+	return res.json('message written'); 
+  });
 });
+
+
+
+
 	
 	
 })
