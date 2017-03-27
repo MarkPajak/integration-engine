2fe9d61f7bb6c0530a92e4c067cf7fd71bd4e46e
Mark Pajak <markpajak.spider@gmail.com>
2017-03-27 10:49:01 +0100

    Update startmongo.bat

--- bin/windows/startmongo.bat
+++ bin/windows/startmongo.bat
@@ -1,1 +1,2 @@
-C:\"Program Files"\MongoDB\Server\3.4\bin\mongod.exe --dbpath "C:\Users\BMAG Tickets\culture-timeline\data" 
+#C:\"Program Files"\MongoDB\Server\3.4\bin\mongod.exe --dbpath "C:\Users\BMAG Tickets\culture-timeline\data" 
+C:\"Program Files"\MongoDB\Server\3.2\bin\mongod.exe --dbpath "C:\data"  --storageEngine=mmapv1
