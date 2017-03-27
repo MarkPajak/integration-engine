982261697887e54cf430cc2f781ce07bf2336d1c
Mark Pajak <markpajak.spider@gmail.com>
2017-03-27 14:30:50 +0100

    Add files via upload

--- bin/windows/startmongo.bat
+++ bin/windows/startmongo.bat
@@ -1,2 +1,1 @@
-#C:\"Program Files"\MongoDB\Server\3.4\bin\mongod.exe --dbpath "C:\Users\BMAG Tickets\culture-timeline\data" 
 C:\"Program Files"\MongoDB\Server\3.2\bin\mongod.exe --dbpath "C:\data"  --storageEngine=mmapv1
