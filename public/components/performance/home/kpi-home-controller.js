d1f2e538620465a687cfe78dcb18e8125229f3ba
MarkPajak <markpajak.spider@gmail.com>
2017-11-27 13:07:55 +0000

    modified:   build/core.js 	new file:   lib/animate_number.js 	modified:   package.json 	modified:   public/bin/core.js 	modified:   public/components/performance/home/kpi-home-controller.js 	modified:   public/components/performance/home/kpi_dashboard.html 	modified:   views/layout.jade

--- public/components/performance/home/kpi-home-controller.js
+++ public/components/performance/home/kpi-home-controller.js
@@ -11,6 +11,28 @@ exports.kpi_home_controller = function(get_kpis,$route,$scope, $http, $q, $route
         x = x.replace(pattern, "$1,$2");
     return x;
 }
+
+function whizz_number(number,id){
+
+	console.log('whizz_number')
+
+$('#'+id).each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: number
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(numberWithCommas(Math.ceil(now)));
+        }
+    });
+});
+		
+		
+		
+}
+
+
 		
 		var current_year=moment(new Date()).format("YYYY")
 		
@@ -31,47 +53,52 @@ exports.kpi_home_controller = function(get_kpis,$route,$scope, $http, $q, $route
 							
 								$scope.visits=numberWithCommas(row.total_sessions) 
 								$scope.visits_num=row.total_sessions
+								console.log('$scope.visits_num',$scope.visits_num)
 							}
 					
 					}
 					
 					if(row._id.ltd_year){
-						
-						
-							
-							
+
 							if((current_year-1).toString()==(row._id.ltd_year).toString() && row._id.ltd_financial_yer=="this"){
 							
 								$scope.lte_visits=numberWithCommas(row.total_sessions) 
 								$scope.lte_visits_num=row.total_sessions
+								console.log('$scope.lte_visits',$scope.lte_visits)
 							}
 							
 					}
-		
-							
+						
 				})
+				
+				whizz_number( $scope.lte_visits_num,'lte_visits-level')
+				whizz_number( $scope.visits_num,'visits-level')
 				
 			$scope.message=""
 		
 				
 			if($scope.lte_visits_num < $scope.visits_num){
-			
-			$scope.message="This is a " + Math.round( (($scope.visits_num / $scope.lte_visits_num)*100)-100 ) +  "% increase compared to this time last year!"
-			
+				
+				$scope.plus=true
+				$scope.percantage= Math.round( (($scope.visits_num / $scope.lte_visits_num)*100)-100 )
+				console.log('$scope.percantage',$scope.percantage)
+				whizz_number($scope.percantage,'diff-level1')
+	
 			}
 			
-			if($scope.lte_visits > $scope.visits){
+			if($scope.lte_visits_num > $scope.visits_num){
 			
-			$scope.message="We have "  +  ($scope.lte_visits_num - $scope.visits_num) +  " to go to beat last year!"
+				$scope.plus=false
+				$scope.percantage= $scope.lte_visits_num - $scope.visits_num
+				console.log('$scope.percantage',$scope.percantage)
+				whizz_number( $scope.percantage,'diff-level2')
+				
 			
 			}
-		})	
 
-}			
+		
 
+		
+})	
 
-
-
-
-
-
+}
