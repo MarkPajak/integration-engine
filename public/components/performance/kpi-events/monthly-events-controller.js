edc21cc9c3cafba03fb8d8b32b56c4b8963a56d0
MarkPajak <markpajak.spider@gmail.com>
2018-08-12 18:49:43 +0100

    modified:   build/core.js 	modified:   public/bin/core.js 	modified:   public/components/performance/kpi-events/monthly-events-controller.js

--- public/components/performance/kpi-events/monthly-events-controller.js
+++ public/components/performance/kpi-events/monthly-events-controller.js
@@ -98,7 +98,7 @@ exports.monthly_kpi_events_controller = function($route,$scope, $http, $q, $rout
 										}	
 												
 												if(column_data[heading_name]){
-												column_data[heading_name]+=parseInt(row[month_year])
+												column_data[heading_name]=parseInt(row[month_year])+parseInt(column_data[heading_name])
 												}
 												else
 												{
