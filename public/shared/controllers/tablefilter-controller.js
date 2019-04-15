638fe65604b630bc83ee39c6c5eb2d857a2daca1
MarkPajak <markpajak.spider@gmail.com>
2019-04-15 10:27:18 +0100

    modified:   NEW YEAR INSTRUCTIONS.readme 	modified:   build/core.js 	modified:   public/bin/core.js 	modified:   public/shared/controllers/tablefilter-controller.js

--- public/shared/controllers/tablefilter-controller.js
+++ public/shared/controllers/tablefilter-controller.js
@@ -1,7 +1,7 @@
 	
 exports.TableFilterController = function(getDateService,$location,AuthService,tableFilterService, $scope,$http,get_table_data) {
 
-  	$scope.start_date=new Date("2018/04/01")
+  	$scope.start_date=new Date("2019/04/01")
 	$scope.end_date= new Date(moment($scope.start_date).add('years', 1).format("YYYY/MM/DD"))
 	 $scope.end_date.setDate($scope.end_date.getDate()-1);
   	$scope.start=	$scope.start_date
